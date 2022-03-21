2562956cf1a20c16fbe21ddd54f189305b7fa2b4
Nguyen Thanh Thuc <nguyen.thanh.rcvn2012@gmail.com>
2022-03-21 14:46:18 +0700

    add

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,8 +14,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-//            UsersTableSeeder::class,
-//            CustomerTableSeeder::class,
+            UsersTableSeeder::class,
+            CustomerTableSeeder::class,
             ProductTableSeeder::class,
         ]);
     }
--- database/seeders/ProductTableSeeder.php
+++ database/seeders/ProductTableSeeder.php
@@ -16,17 +16,20 @@ class ProductTableSeeder extends Seeder
     {
         $faker = \Faker\Factory::create();
 
-        for ($i = 0; $i < 98; $i++) {
+        for ($i = 0; $i < 30; $i++) {
             $name = $faker->name;
             $id = getIdProduct($name);
 
             DB::table('products')->insert([
                 'product_id' => $id,
                 'product_name' => $name,
+                'product_image' =>  $faker->image('public/uploads/products', 400, 300, null, false),
                 'product_price' => (int) '1200000'.$i,
                 'description' => $faker->paragraph(1),
                 'created_at' =>  \Carbon\Carbon::now()
             ]);
+
+            sleep(0.5);
         }
     }
 }
